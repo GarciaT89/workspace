f28098783fb3f1768096e31ea8782ff541e3f6c2
GarciaT89 <c180259@ccg.ac.jp>
2019-10-28 03:18:58 +0000

    programming building class

---
+++ ccmc-03/useBuilding.php
@@ -0,0 +1,36 @@
+<?php
+require_once("class/Building.php");
+
+$kasumigaseki = new Building("霞が関ビル", 147);
+$subshine = new Building("サブシャイン60", 240);
+
+echo "<pre>";
+var_dump($kasumigaseki);
+echo "</pre>";
+
+exit(0);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Buildingクラスを利用する</title>
+	<link rel="stylesheet" href="../assets/css/style.css" />
+	<link rel="stylesheet" href="../assets/css/ccmc-03.css" />
+</head>
+
+<body>
+	<h1>Buildingクラスを利用する</h1>
+	<dl>
+		<dt><?=$kasumigaseki->getName() ?></dt>
+		<dd><?=$kasumigaseki->getHeight() ?></dd>
+	</dl>
+	<dl>
+		<dt><?=$subshine->getName() ?></dt></dt>
+		<dd><?=$subshine->getHeight() ?></dd>
+	</dl>
+	<p><a href="index.html">インデックスに戻る</a></p>
+</body>
+
+</html>
