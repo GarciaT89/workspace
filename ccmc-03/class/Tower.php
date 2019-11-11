34990ff6796333232ec60b3987a7abe7269e73e9
GarciaT89 <c180259@ccg.ac.jp>
2019-11-11 08:32:18 +0000

    2019-11-11

---
+++ ccmc-03/class/Tower.php
@@ -0,0 +1,19 @@
+<?php
+require_once("Building.php");
+class Tower extends Building{
+    /**属性**/
+    private $buitYear;
+    
+    /**コンストラクタ**/
+    function __construct(string $name, int $height, int $buitYear){
+        parent::__construct($name, $height);
+        $this->builtYear = $buitYear;
+    }
+    function getProfile():string{
+        $profile = "{$this->name}の高さは{$this->height}mで、{$this->builtYear} 年に開業しました。";
+        return $profile;
+    
+    }
+    
+}
+?>
